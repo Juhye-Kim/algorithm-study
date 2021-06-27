fbd171ac397934da639ba26c6c64e16b120e52a4
Juhye-Kim <kjh960215@gmail.com>
2021-06-27 16:43:39 +0900

    프로그래머스 K번째수

---
+++ Programmers/K번째수.js
@@ -0,0 +1,7 @@
+function solution(array, commands) {
+  const answer = [];
+  commands.forEach((el) => {
+    answer.push(array.slice(el[0] - 1, el[1]).sort((a, b) => a - b)[el[2] - 1]);
+  });
+  return answer;
+}
