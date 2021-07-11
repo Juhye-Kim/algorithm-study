5bfa558736f8ec2f44defb1659a5669bb4daaa1d
Juhye-Kim <kjh960215@gmail.com>
2021-07-11 19:34:12 +0900

    프로그래머스 완주하지 못한 선수 ver2

--- Programmers/Level1/완주하지못한선수.js
+++ Programmers/Level1/완주하지못한선수.js
@@ -1,3 +1,16 @@
+/* 프로그래머스 - 완주하지 못한 선수 (해시) */
+
+// ver2
+function solution(participant, completion) {
+  let parts = {};
+  participant.forEach((p) => (parts[p] ? parts[p]++ : (parts[p] = 1)));
+  completion.forEach((c) => parts[c]--);
+  for (let i in parts) {
+    if (parts[i] > 0) return i;
+  }
+}
+
+// ver1
 function solution(participant, completion) {
   var answer = "";
   let arr = {};
