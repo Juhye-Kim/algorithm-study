015732a0b1d82188a1105dbe53c47d92cd92c33f
Juhye-Kim <kjh960215@gmail.com>
2021-07-12 14:23:31 +0900

    Codility MaxCounter (ver3)

--- Codility/MaxCounter.js
+++ Codility/MaxCounter.js
@@ -14,7 +14,7 @@ function solution(N, A) {
   return arr;
 }
 
-// re
+// ver2
 function solution(N, A) {
   let arr = Array(N).fill(0);
   let maxCount = 0;
@@ -32,4 +32,22 @@ function solution(N, A) {
     if (el < maxCount) return maxCount;
     else return el;
   });
+}
+
+// ver3
+function solution(N, A) {
+  let [max, maxCnt] = [0, 0];
+  let arr = Array(N + 1).fill(0);
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === N + 1) maxCnt = max;
+    else {
+      arr[A[i]] = Math.max(maxCnt, arr[A[i]]);
+      arr[A[i]]++;
+      max = Math.max(max, arr[A[i]]);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.max(maxCnt, arr[i]);
+  }
+  return arr.slice(1);
 }
