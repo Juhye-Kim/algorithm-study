bfab1e0fcdcfa42793d83477a50ed1976c98f218
Juhye-Kim <kjh960215@gmail.com>
2021-07-12 11:17:55 +0900

    LeetCode 3. Longest Substring Without Repeating Characters (ver2)

--- LeetCode/LongestSubstringWithout.js
+++ LeetCode/LongestSubstringWithout.js
@@ -1,5 +1,6 @@
 /* LeetCode 3. Longest Substring Without Repeating Characters (문자열) */
 
+// ver1. 412ms
 var lengthOfLongestSubstring = function (s) {
   let ans = 0;
   let i = 0;
@@ -14,6 +15,23 @@ var lengthOfLongestSubstring = function (s) {
     }
     ans = Math.max(ans, cnt);
     i++;
+  }
+  return ans;
+};
+
+// ver2. 216ms
+
+var lengthOfLongestSubstring = function (s) {
+  let ans = 0;
+  let exist = {};
+  let right = 0;
+  for (let left = 0; left < s.length; left++) {
+    while (right < s.length && !exist[s[right]]) {
+      exist[s[right]] = true;
+      right++;
+    }
+    ans = Math.max(ans, right - left);
+    exist[s[left]] = false;
   }
   return ans;
 };
