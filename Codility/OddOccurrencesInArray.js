ed0308536bef26824be0cc93f22e9a7d28ca720d
Juhye-Kim <kjh960215@gmail.com>
2021-07-12 12:42:34 +0900

    Codility Odd OcurrencesInArray (ver2)

--- Codility/OddOccurrencesInArray.js
+++ Codility/OddOccurrencesInArray.js
@@ -10,3 +10,15 @@ function solution(A) {
     if (obj[key] % 2 !== 0) return Number(key);
   }
 }
+
+function solution(A) {
+  let hash = {};
+  for (let i in A) {
+    hash[A[i]] === undefined
+      ? (hash[A[i]] = false)
+      : (hash[A[i]] = !hash[A[i]]);
+  }
+  for (let key in hash) {
+    if (!hash[key]) return Number(key);
+  }
+}
