b4e69616b136cba1003cc9332115d312cd327a91
Juhye-Kim <kjh960215@gmail.com>
2021-07-13 21:42:33 +0900

    Codility StoneWall (ver2)

--- Codility/StoneWall.js
+++ Codility/StoneWall.js
@@ -1,3 +1,5 @@
+/* Codility StoneWall */
+// ver1
 function solution(H) {
   let cnt = 1;
   let stack = [H[0]];
@@ -14,4 +16,20 @@ function solution(H) {
     i++;
   }
   return cnt;
+}
+
+// ver 2
+function solution(H) {
+  let ans = 1;
+  let stack = [H[0]];
+  for (let i = 1; i < H.length; i++) {
+    while (stack.length && stack[stack.length - 1] > H[i]) {
+      stack.pop();
+    }
+    if (stack[stack.length - 1] !== H[i]) {
+      stack.push(H[i]);
+      ans++;
+    }
+  }
+  return ans;
 }
