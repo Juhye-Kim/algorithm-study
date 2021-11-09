aee0d5b932f26df8011ed2c74321827abdd287c4
Juhye-Kim <kjh960215@gmail.com>
2021-11-09 14:37:30 +0900

    프로그래머스 문자열 압축

--- Programmers/Level2/문자열압축.js
+++ Programmers/Level2/문자열압축.js
@@ -1,28 +1,23 @@
-// 프로그래머스 문자열 압축
-
 function solution(s) {
   let min = s.length;
-  for (let i = 1; i <= parseInt(s.length / 2) + 1; i++) {
-    // i = 압축 단위
-    let full = s;
-    let j = 0; // j = 압축대상 포인터
-    while (full[j]) {
-      let cnt = 1;
-      let str = full.substr(j, i);
-      let k = j + i; // k = 비교대상 포인터
-      while (full[k]) {
-        let sub = full.substr(k, i);
-        if (str === sub) {
-          cnt++;
-          k += i;
-        } else break;
+
+  for (let i = 1; i <= parseInt(s.length / 2); i++) {
+    let len = s.length;
+    let cur = s.substring(0, i);
+    let cnt = 1;
+
+    for (let j = i; j <= s.length; j += i) {
+      const str = s.substring(j, j + i);
+
+      if (cur === str) cnt++;
+      else {
+        len -= i * (cnt - 1);
+        if (cnt > 1) len += (cnt + "").length;
+        [cur, cnt] = [str, 1];
       }
-      if (cnt > 1) {
-        full = full.substr(0, j) + cnt + str + full.substr(j + cnt * i);
-        j += i + cnt.toString().length;
-      } else j += i;
-      min = Math.min(min, full.length);
     }
+
+    if (len < min) min = len;
   }
   return min;
 }
