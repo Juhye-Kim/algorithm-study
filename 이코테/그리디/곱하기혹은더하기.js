94ce9abf879cf17cc1e89178fe4064f66d1b5add
Juhye-Kim <kjh960215@gmail.com>
2021-11-23 18:17:36 +0900

    이코테 - 곱하기 혹은 더하기

--- 이코테/그리디/곱하기혹은더하기.js
+++ 이코테/그리디/곱하기혹은더하기.js
@@ -1,19 +1,30 @@
 /* 곱하기 혹은 더하기 (그리디) */
 
 function solution(input) {
-  let max = 0;
   let nums = input.split("").map((el) => Number(el));
-  calc(1, nums[0]);
-  function calc(step, output) {
-    if (step === input.length) {
-      max = Math.max(max, output);
-      return;
-    }
-    calc(step + 1, output + nums[step]);
-    calc(step + 1, output * nums[step]);
-  }
-  return max;
+
+  return nums.reduce((prev, cur) => {
+    const sum = prev + cur;
+    const multi = prev * cur;
+
+    return Math.max(sum, multi);
+  });
 }
+
+// function solution(input) {
+//   let max = 0;
+//   let nums = input.split("").map((el) => Number(el));
+//   calc(1, nums[0]);
+//   function calc(step, output) {
+//     if (step === input.length) {
+//       max = Math.max(max, output);
+//       return;
+//     }
+//     calc(step + 1, output + nums[step]);
+//     calc(step + 1, output * nums[step]);
+//   }
+//   return max;
+// }
 
 console.log(solution("02984")); // 576
 console.log(solution("567")); // 210
